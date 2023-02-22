f91e933ae3cde32861a619b0a99d8af216df28e8
Ascold2017 <mihail.pogosyan96@gmail.com>
2023-02-22 13:40:53 +0200

    Update radar

--- src/Core/Radar.ts
+++ src/Core/Radar.ts
@@ -1,4 +1,4 @@
-import { WebGLRenderer, OrthographicCamera } from "three";
+import { WebGLRenderer, OrthographicCamera, Scene, WebGLRenderTarget, Mesh, PlaneGeometry, MeshBasicMaterial, MeshStandardMaterial, ShaderMaterial } from "three";
 import { Layer } from "konva/lib/Layer";
 import { Circle } from "konva/lib/shapes/Circle";
 import { Line } from "konva/lib/shapes/Line";
@@ -10,25 +10,106 @@ import WorldScene from "./WorldScene";
 export default class Radar extends LoopHelper {
     private readonly worldScene: WorldScene;
     private readonly renderer: WebGLRenderer;
-    private cameraPPI: OrthographicCamera | null = null;
+    private readonly camera: OrthographicCamera = new OrthographicCamera(-400, 400, -400, 400, 0, 10);
+
+    private ppiScreenRenderTarget: WebGLRenderTarget | null = null;
+    private ppiScreenScene: Scene | null = null;
+    private ppiCamera: OrthographicCamera | null = null;
     private rotationPPI = 0;
-    private readonly antennaHeight = 35;
-    private gain = 0.9;
     constructor(container: string) {
         super();
         const containerEl = document.getElementById(container);
         this.worldScene = new WorldScene();
+
         this.renderer = new WebGLRenderer({ alpha: true });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(800, 800);
-        this.setupCameraPPI();
+        this.setupPPI();
         this.initFlyObjects();
+
         containerEl?.appendChild(this.renderer.domElement);
         containerEl?.appendChild(this.createIndicatorScreen());
+        this.camera.position.y = 5;
+        this.camera.rotateX(-180 * (Math.PI / 180));
+        this.updatePPI();
+        setInterval(() => this.updatePPI(), 5000)
         this.addLoop('renderLoop', () => {
-            this.updatePPI();
-            this.renderer.render(this.worldScene.scene, this.cameraPPI!);
+            this.rotationPPI += 1;
+            if (this.rotationPPI >= 360) {
+                this.rotationPPI = 0;
+            }
+            this.renderer.render(this.ppiScreenScene!, this.camera);
+
         })
+    }
+
+    setupPPI() {
+
+        this.ppiCamera = new OrthographicCamera(-this.worldScene.mapSize / 2, this.worldScene.mapSize / 2, -this.worldScene.mapSize / 2, this.worldScene.mapSize / 2, 0, 200);
+        this.ppiCamera.position.set(0, 0, 100);
+
+        this.ppiScreenRenderTarget = new WebGLRenderTarget(800, 800);
+
+        this.ppiScreenScene = new Scene();
+        const shaderMaterial = new ShaderMaterial({
+            uniforms: {
+                uRadius: { value: 400 },
+                uRotation: { value: 0 },
+                uTexture: { value: this.ppiScreenRenderTarget.texture }
+            },
+            vertexShader: `
+                uniform float uRotation;
+                varying vec2 vUv;
+                varying float vAngleCoef;
+                varying float dx;
+                varying float dy;
+                const float PI = 3.14159265358979;
+
+                void main() {
+                    vUv = uv;
+                    vec4 worldPosition = vec4(modelMatrix * viewMatrix * vec4(position, 1.0));
+                    dx = position.x;
+                    dy = position.y;
+                    vec3 lightDirection = normalize(vec3(dx,dy,0));
+
+                    vec3 angleDirection = vec3(sin(uRotation), cos(uRotation), 0.0);
+
+                    vec3 p = cross(angleDirection, lightDirection);
+                    // angle b/w vectors
+                    float s = dot(angleDirection, lightDirection);
+                    float ang = acos(s) * 180.0 / PI;
+                    // bring to 0..360
+                    if (p.z < 0.0) ang = 360.0 - ang;
+                    // bring to 0..1
+                    vAngleCoef = (1.0 - ang / 360.0);
+                    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+                }
+            `,
+            fragmentShader: `
+                precision highp float;
+                uniform float uRadius;
+                uniform sampler2D uTexture;
+                varying vec2 vUv;
+                varying float vAngleCoef;
+                varying float dx;
+                varying float dy;
+                void main() {
+                    float r = sqrt(dx * dx + dy * dy);
+                    if (r < uRadius) {
+                        gl_FragColor = vec4(texture2D(uTexture, vUv).rgb * vAngleCoef, 1.0);
+                    } else {
+                        discard;
+                    }
+                    
+                }
+            `
+        })
+        const screenPlane = new Mesh(new PlaneGeometry(800, 800, 799, 799), shaderMaterial);
+        this.addLoop('rotationLoop', () => {
+            shaderMaterial.uniforms.uRotation.value = this.rotationPPI * (Math.PI / 180);
+        })
+        this.ppiScreenScene.add(screenPlane);
+
     }
 
     initFlyObjects() {
@@ -40,25 +121,18 @@ export default class Radar extends LoopHelper {
 
         data.forEach(i => {
             const t = this.worldScene.addTarget(i.name, i.position, i.visibilityK);
-            this.addFixedLoop(i.name, 7, () => {
-                t.position.x += 3;
-                t.position.y += 3;
+            this.addLoop(i.name, () => {
+                t.position.x += 0.3;
+                t.position.y += 0.3;
 
             })
         })
     }
 
-    private setupCameraPPI() {
-        this.cameraPPI = new OrthographicCamera(-this.worldScene.mapSize / 2, this.worldScene.mapSize / 2, -this.worldScene.mapSize / 2, this.worldScene.mapSize / 2, 0, 200);
-        this.cameraPPI.position.set(0, 0, 100);
-    }
-
     private updatePPI() {
-        this.rotationPPI += 1;
-        if (this.rotationPPI >= 360) {
-            this.rotationPPI = 0;
-        }
-        this.worldScene.updateUniforms({ rotation: this.rotationPPI, gain: this.gain, antennaHeight: this.antennaHeight })
+        this.renderer.setRenderTarget(this.ppiScreenRenderTarget);
+        this.renderer.render(this.worldScene.scene, this.ppiCamera!);
+        this.renderer.setRenderTarget(null);
     }
 
 
--- src/Core/WorldScene.ts
+++ src/Core/WorldScene.ts
@@ -6,6 +6,8 @@ import TerrainGenerator from "./TerrainGenerator";
 export default class WorldScene {
     readonly scene = new Scene();
     readonly mapSize = 1000;
+    private gain = 0.9;
+    private radarHeight = 15;
 
     constructor() {
         this.initScene()
@@ -21,18 +23,15 @@ export default class WorldScene {
     addTarget(name: string, position: { x: number, y: number, z: number }, visibilityK: number) {
         const shaderMaterial = new ShaderMaterial({
             uniforms: {
-                uLightPosition: { value: [0, 0, 0] },
-                uGain: { value: 0 },
-                uAngle: { value: 0 },
+                uLightPosition: { value: [0, 0, -this.radarHeight] },
+                uGain: { value: this.gain},
                 uRange: { value: this.mapSize / 2 },
                 uVisibilityK: { value: visibilityK }
             },
             vertexShader: `
                 uniform vec3 uLightPosition;
-                uniform float uAngle;
                 uniform float uVisibilityK;
                 varying float vLightWeighting;
-                varying float vAngleCoef;
                 varying float dx;
                 varying float dy;
                 const float PI = 3.14159265358979;
@@ -41,23 +40,12 @@ export default class WorldScene {
                     vec4 worldPosition = vec4(modelMatrix * vec4(position, 1.0));
                     dx = worldPosition.x - uLightPosition.x;
 	                dy = worldPosition.y - uLightPosition.y;
-                    float angleRad = uAngle * PI / 180.0;
-                    vec3 angleDirection = vec3(sin(angleRad), cos(angleRad), 0.0);
                     vec3 lightDirection = normalize(vec3(dx,dy,0));
                     
                     float height = -uLightPosition.z;
                     float coef = worldPosition.z / height;
                     if (coef > 1.0) coef = 1.0;
                     vLightWeighting = coef * uVisibilityK;
-
-                    vec3 p = cross(angleDirection,lightDirection);
-                    // angle b/w vectors
-                    float s = dot(angleDirection, lightDirection);
-                    float ang = acos(s) * 180.0 / PI;
-                    // bring to 0..360
-                    if (p.z < 0.0) ang = 360.0 - ang;
-                    // bring to 0..1
-                    vAngleCoef = (1.0 - ang / 360.0);
 
                     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
                     
@@ -67,7 +55,6 @@ export default class WorldScene {
                 uniform float uRange;
                 uniform float uGain;
                 varying float vLightWeighting;
-                varying float vAngleCoef;
                 varying float dx;
                 varying float dy;
                 void main() {
@@ -75,10 +62,10 @@ export default class WorldScene {
                     if (r > uRange) {
                         discard;
                     } else {
-                        if (vLightWeighting * vAngleCoef <= (1.0 - uGain)) {
+                        if (vLightWeighting <= (1.0 - uGain)) {
                             gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
                         } else {
-                            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0) * vLightWeighting * vAngleCoef;
+                            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0) * vLightWeighting;
                         }
                     }
                     
@@ -97,6 +84,7 @@ export default class WorldScene {
         const terrainGeometry = new PlaneGeometry(this.mapSize, this.mapSize, widthSegments - 1, heightSegments - 1);
         // Apply height data to plane
         data.forEach((h, i) => {
+            // @ts-ignore
             terrainGeometry.attributes.position.array[i * 3 + 2] = h; // set z-coordinate
         });
         // Update normals
@@ -107,35 +95,21 @@ export default class WorldScene {
 
         const shaderMaterial = new ShaderMaterial({
             uniforms: {
-                uLightPosition: { value: [0, 0, 0] },
-                uGain: { value: 0 },
-                uAngle: { value: 0 },
+                uLightPosition: { value: [0, 0, -this.radarHeight] },
+                uGain: { value: this.gain},
                 uRange: { value: this.mapSize / 2 }
             },
             vertexShader: `
                 uniform vec3 uLightPosition;
-                uniform float uAngle;
                 varying float vLightWeighting;
-                varying float vAngleCoef;
                 varying float dx;
                 varying float dy;
                 const float PI = 3.14159265358979;
                 void main() {
                     dx = position.x - uLightPosition.x;
 	                dy = position.y - uLightPosition.y;
-                    float angleRad = uAngle * PI / 180.0;
-                    vec3 angleDirection = vec3(sin(angleRad),cos(angleRad),0.0);
                     vec3 lightDirection = normalize(vec3(dx,dy,0));
                     vLightWeighting = -dot(normal,lightDirection);
-
-                    vec3 p = cross(angleDirection,lightDirection);
-                    // angle b/w vectors
-                    float s = dot(angleDirection, lightDirection);
-                    float ang = acos(s) * 180.0 / PI;
-                    // bring to 0..360
-                    if (p[2] < 0.0) ang = 360.0 - ang;
-                    // bring to 0..1
-                    vAngleCoef = (1.0 - ang / 360.0);
 
                     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
@@ -145,7 +119,6 @@ export default class WorldScene {
                 uniform float uRange;
                 uniform float uGain;
                 varying float vLightWeighting;
-                varying float vAngleCoef;
                 varying float dx;
                 varying float dy;
                 void main() {
@@ -153,10 +126,10 @@ export default class WorldScene {
                     if (r > uRange) {
                         discard;
                     } else {
-                        if (vLightWeighting * vAngleCoef <= (1.0 - uGain)) {
+                        if (vLightWeighting <= (1.0 - uGain)) {
                             gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
                         } else {
-                            vec3 finalColor = vec3(1.0, 1.0, 0.0) * vLightWeighting * vAngleCoef;
+                            vec3 finalColor = vec3(1.0, 1.0, 0.0) * vLightWeighting;
                             gl_FragColor = vec4(finalColor, 1.0);
                         }
                     }
@@ -169,13 +142,5 @@ export default class WorldScene {
         terrainMesh.rotateX(180 * (Math.PI / 180))
 
         this.scene.add(terrainMesh);
-    }
-
-    updateUniforms({ rotation, gain, antennaHeight }: { rotation: number, gain: number, antennaHeight: number}) {
-        this.scene.children.filter(o => o.isMesh).forEach((mesh: Mesh) => {
-            mesh.material.uniforms.uAngle.value = rotation;
-            mesh.material.uniforms.uGain.value = gain;
-            mesh.material.uniforms.uLightPosition.value = [0, 0, -antennaHeight];
-        });
     }
 }
